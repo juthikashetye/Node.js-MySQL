b1a716c04fa9f031b32a20d9752023694525fa97
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-03-25 19:02:30 -0700

    manager done, modified customer

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -26,8 +26,7 @@ function getProductDetails() {
 
     if (err) throw err;
 
-    // console.log(result);
-    console.log("\n Welcome to Bamazon! Which is BETTER than AMAZON. \n");
+    console.log("\n Welcome to Bamazon! Which is a BETTER AMAZON. \n");
 
     createTable(result);
 
@@ -57,35 +56,33 @@ function promptUserToBuy() {
 
   inquirer
     .prompt([{
-        type: "input",
+        type: "number",
         message: "\n Enter the ID of the product you want to buy.",
         name: "selectedId"
       },
       {
-        type: "input",
+        type: "number",
         message: "\n Enter the quantity you want to buy.",
         name: "quantity"
       }
     ])
-    .then(function(inquirerResponse) {
+    .then(function(prompt) {
 
       //selecting & showing the product details
-      con.query("SELECT product_name, price, stock_quantity FROM products WHERE id = ?", [inquirerResponse.selectedId], function(err, res) {
+      con.query("SELECT product_name, price, stock_quantity FROM products WHERE id = ?", [prompt.selectedId], function(err, res) {
 
-        console.log("\n" + "You requested for " + inquirerResponse.quantity + " " + res[0].product_name + " which costs $" + res[0].price + " each." + "\n");
+        console.log("\n" + "You requested for " + prompt.quantity + " " + res[0].product_name + " which costs $" + res[0].price + " each." + "\n");
 
         //checking for stock in store
         //if enough stock then update database by subtracting the product quantity requested by user
-        if (res[0].stock_quantity >= inquirerResponse.quantity) {
+        if ((res[0].stock_quantity >= prompt.quantity)) {
 
-          var updatedStock = res[0].stock_quantity - parseInt(inquirerResponse.quantity);
+          var updatedStock = res[0].stock_quantity - parseInt(prompt.quantity);
 
-          // console.log("Items remaining in stock " + updatedStock + "\n");
-
-          var payment = res[0].price * inquirerResponse.quantity;
+          var payment = res[0].price * prompt.quantity;
 
           //query for updating database
-          con.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [updatedStock, inquirerResponse.selectedId], function(err, response) {
+          con.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [updatedStock, prompt.selectedId], function(err, response) {
 
             console.log("Your Order was successfully placed." + "\n \n" +
               "The total amount to be paid is $" + payment + "\n");
@@ -93,15 +90,14 @@ function promptUserToBuy() {
 
           });
 
-          // console.log(res[0].stock_quantity);
-
           //if stock has less items than requested then show this msg
-        } else if ((res[0].stock_quantity < inquirerResponse.quantity) && (!res[0].stock_quantity == 0)) {
+        } else if ((res[0].stock_quantity < prompt.quantity) && (!res[0].stock_quantity == 0)) {
 
           console.log("Sorry we couldn't complete your order for " + res[0].product_name + " as we have only " + res[0].stock_quantity + " in stock." + "\n");
           continueShopping();
+
           //if stock has 0 items for the requested product then show this msg 
-        } else if (res[0].stock_quantity === 0) {
+        } else if (res[0].stock_quantity == 0) {
 
           console.log("Sorry we are Out of Stock for " + res[0].product_name + "." + "\n");
           continueShopping();
@@ -125,7 +121,7 @@ function continueShopping() {
       if (userResponse.shopMore == "YES") {
         getProductDetails();
       } else {
-        console.log("\n Thank you for visiting Bamazon! See you soon again. \n");
+        console.log("\n Thank you for visiting Bamazon! See you soon again. Bye! \n");
         con.end();
       }
 
---
+++ bamazon_manager.js
@@ -0,0 +1,240 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table2");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "", //add MySQl password
+  database: "bamazon"
+});
+
+function createTable(data) {
+
+  // By default, headers will be red, and borders will be grey
+  var table = new Table({
+    head: ["ID", "Product", "Department", "Price", "In Stock"],
+    wordWrap: true
+  });
+
+  for (var i = 0; i < data.length; i++) {
+    table.push([data[i].id, data[i].product_name, data[i].department_name, "$" + data[i].price, data[i].stock_quantity]);
+  }
+
+  console.log(table.toString());
+
+}
+
+function showMenu() {
+
+  //prompt manager to select a task
+  inquirer
+    .prompt([{
+      type: "list",
+      message: "\n Hi, what do you want to do?",
+      choices: ["View Products For Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "I am done. Log me out"],
+      name: "task"
+    }])
+    .then(function(mngr) {
+
+      switch (mngr.task) {
+        case "View Products For Sale":
+          viewProducts();
+          break;
+        case "View Low Inventory":
+          viewLowStock();
+          break;
+        case "Add to Inventory":
+          addToStock();
+          break;
+        case "Add New Product":
+          addNewProduct();
+          break;
+        case "I am done. Log me out":
+          logOut();
+          break;
+      }
+    });
+}
+
+function viewProducts() {
+
+  //query all products for the manager to view
+  con.query("SELECT * FROM products", function(err, result, fields) {
+
+    if (err) throw err;
+
+    console.log("\n Here's a list of all the products in store. \n");
+
+    createTable(result);
+    continueManaging();
+
+  });
+}
+
+function viewLowStock() {
+
+  //query for products that have less than 5 items in stock
+  con.query("SELECT * FROM products WHERE stock_quantity < ?", [5], function(err, result, fields) {
+
+    if (err) throw err;
+
+    //if there are items low on stock then do this
+    if (result.length > 0) {
+
+      console.log("\n The following products are low in stock. \n");
+
+      createTable(result);
+      continueManaging();
+
+    } //if result set is empty/no items less than 5 in stock then do this
+    else {
+      console.log("\n Your inventory is well stocked. \n");
+      continueManaging();
+    }
+
+  });
+}
+
+function addToStock() {
+
+  //show all products to manager for reference
+  con.query("SELECT * FROM products", function(err, result, fields) {
+
+    if (err) throw err;
+
+    console.log("\n Here's a list of all the products in store for your reference. \n");
+
+    createTable(result);
+
+    //prompt manager to add items in stock
+    inquirer
+      .prompt([{
+          type: "input",
+          message: "\n Enter the ID of the product you want to add more of in the stock.",
+          name: "prodId"
+        },
+        {
+          type: "input",
+          message: "\n Enter the quantity you want to add in stock.",
+          name: "quantityAdded"
+        }
+      ])
+      .then(function(update) {
+
+      	//only select the item which is being updated for making a addedStock variable
+        con.query("SELECT * FROM products WHERE id = ?", [update.prodId], function(err, result, fields) {
+
+          if (err) throw err;
+
+          var addedStock = result[0].stock_quantity + parseInt(update.quantityAdded);
+
+          //query for updating database
+          con.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [addedStock, update.prodId], function(err, result, fields) {
+
+            if (err) throw err;
+
+            console.log("\n The inventory was successfully updated. \n");
+
+            //show the row of product which was updated
+            con.query("SELECT * FROM products WHERE id = ?", [update.prodId], function(err, result, fields) {
+
+              if (err) throw err;
+
+              createTable(result);
+              continueManaging();
+            });
+
+          });
+
+        });
+      });
+
+  });
+}
+
+function addNewProduct() {
+
+  //show all products to manager for reference
+  con.query("SELECT * FROM products", function(err, result, fields) {
+
+    if (err) throw err;
+
+    console.log("\n Here's a list of all the products in store for your reference. \n");
+
+    createTable(result);
+
+    //prompt manager to add new product
+    inquirer
+      .prompt([{
+          type: "input",
+          message: "\n Enter the name of the new product.",
+          name: "prodName"
+        },
+        {
+          type: "input",
+          message: "\n Enter the department of the product.",
+          name: "dept"
+        },
+        {
+          type: "number",
+          message: "\n Enter the price of the product.",
+          name: "prodPrice"
+        },
+        {
+          type: "number",
+          message: "\n Enter the quantity of the product.",
+          name: "stock"
+        }
+      ])
+      .then(function(newProd) {
+
+      	//query to insert new row in product table. Note the separate '?' for each VALUE
+        con.query("INSERT INTO products (product_name, department_name,price,stock_quantity) VALUES (?,?,?,?)",
+          [newProd.prodName, newProd.dept, newProd.prodPrice, newProd.stock],
+          function(err, result, fields) {
+
+            if (err) throw err;
+
+            console.log("\n The product was successfully added to the store. See the new entry included in the table below. \n");
+
+            //show the new product included with the entire table
+            con.query("SELECT * FROM products", function(err, result, fields) {
+
+              if (err) throw err;
+
+              createTable(result);
+              continueManaging();
+            });
+          });
+      });
+  });
+}
+
+function continueManaging() {
+
+  //ask if manager wants to continue working
+  inquirer
+    .prompt([{
+      type: "list",
+      message: "\n Do you want to continue working?",
+      choices: ["YES", "NO"],
+      name: "manageMore"
+    }])
+    .then(function(userResponse) {
+
+      if (userResponse.manageMore == "YES") {
+        showMenu();
+      } else {
+        logOut();
+      }
+
+    });
+}
+
+function logOut() {
+  console.log("\n Thank you for visiting Bamazon! See you soon again. Bye! \n");
+  con.end();
+}
+
+showMenu();
